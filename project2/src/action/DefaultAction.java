affb69f8685517ac9fbefea0a1df89950d8ade36
breakin84 <breakin84@naver.com>
2016-10-18 20:38:23 +0900

    DocFairy
    01

---
+++ project2/src/action/DefaultAction.java
@@ -0,0 +1,18 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DefaultAction extends ActionSupport{
+
+	public String index()throws Exception{
+		return "success";
+	}
+	
+	public String card() throws Exception{
+		return "success";
+	}
+	public String join() throws Exception{
+		return "success";
+	}
+
+}
---
+++ project2/src/action/DocumentAction.java
@@ -0,0 +1,17 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DocumentAction extends ActionSupport {
+	
+	public String docTransform()throws Exception{
+		return "success";
+	}
+	public String docIntegrate()throws Exception{
+		return "success";
+	}
+	public String docForm()throws Exception{
+		return "success";
+	}
+
+}
