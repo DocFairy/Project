e992a6a8536ee9f1215ffd2871680ab89379a5b3
storm <storm@DESKTOP-JJ9R7LK>
2016-10-27 20:02:12 +0900

    ㄴ

--- src/excel/PathSaver.java
+++ src/excel/PathSaver.java
@@ -6,9 +6,12 @@ public class PathSaver {
 		
 	}
 	
+	//"C:/ServerUtils/workspace_projectDocFairy/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Project
+	//이게 저장되어 있는 주소라고 친다면, 
+	//C:/ServerUtils/workspace_projectDocFairy/ 이곳이 자신의 eclipse의 workspace의 주소입니다.
 	public static String filePath(){ //톰캣에 올라가진 실제 파일 주소
 		return "C:/ServerUtils/workspace_projectDocFairy/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Project";
-	}//C:/ServerUtils/workspace_projectDocFairy/ 이곳이 자신의 eclipse의 workspace의 주소입니다.
+	}
 	
 	public static String librePath(){ //libreOffice가 저장된 주소
 		return "C:/Program Files (x86)/LibreOffice 5";
