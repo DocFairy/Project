f5234e51c5078c4c6c6eaf0dee72f8735ef1922a
breakin84 <breakin84@naver.com>
2016-10-27 14:06:10 +0900

    eeee

--- src/action/DocumentAction.java
+++ src/action/DocumentAction.java
@@ -14,16 +14,21 @@ import com.opensymphony.xwork2.ActionSupport;
 import dao.DocumentDAO;
 import excel.Converter2;
 import excel.ExcelMain;
+import excel.ExcelReadWrite;
 import excel.ReadExcelDemo;
+import excel.ReadWord;
 import vo.Files;
 import vo.Members;
 
-public class DocumentAction extends ActionSupport implements SessionAware{
+public class DocumentAction extends ActionSupport implements SessionAware {
 	Files files;
-	private File upload;					/** 업로드할 파일. Form의 <file> 태그의 name. */
-	private String uploadFileName;			/** 업로드할 파일의 파일명 (File타입 속성명 + "FileName") */
-	private String uploadContentType;		/** 업로드할 파일의 컨텐츠 타입 (File타입 속성명 + "ContentType") */
-	private List<Files>list;
+	private File upload;
+	/** 업로드할 파일. Form의 <file> 태그의 name. */
+	private String uploadFileName;
+	/** 업로드할 파일의 파일명 (File타입 속성명 + "FileName") */
+	private String uploadContentType;
+	/** 업로드할 파일의 컨텐츠 타입 (File타입 속성명 + "ContentType") */
+	private List<Files> list;
 	private Map<String, Object> session;
 	private Members members;
 	private List<Files> docFormList;
@@ -33,174 +38,211 @@ public class DocumentAction extends ActionSupport implements SessionAware{
 	private String integrate;
 	private String msg;
 	private String filename_pdf;
-//	private DocCustomizing aNewCreate;
-	private List<Files> createFiles; 
+	// private DocCustomizing aNewCreate;
+	private List<Files> createFiles;
 	private String arr;
 	private String[] createList;
-	//파일 만들기만.
-/*	public String docCreate(){
-		System.out.println("length : " + arr);
-		System.out.println(aNewCreate.toString());
-		new OpenExcelFile().createExcelDoc(aNewCreate, arr);
-//		createFiles = new DocumentDAO().customizingDocCreate(
-//						new OpenExcelFile().createExcelDoc(aNewCreate));
-		return SUCCESS;
-	}
-	//공유시 데이터베이스에 업로드.
-	public String docShare(){
-		return SUCCESS;
-	}
-	public String customizingList(){
-		createList = DocCustomizing.list;
-		return SUCCESS;
-	}*/
+	// 파일 만들기만.
+	/*
+	 * public String docCreate(){ System.out.println("length : " + arr);
+	 * System.out.println(aNewCreate.toString()); new
+	 * OpenExcelFile().createExcelDoc(aNewCreate, arr); // createFiles = new
+	 * DocumentDAO().customizingDocCreate( // new
+	 * OpenExcelFile().createExcelDoc(aNewCreate)); return SUCCESS; } //공유시
+	 * 데이터베이스에 업로드. public String docShare(){ return SUCCESS; } public String
+	 * customizingList(){ createList = DocCustomizing.list; return SUCCESS; }
+	 */
 
-	
-	
-	
-	
-	
-	
-	
-	
-	public String docTransform() throws Exception{
+	public String docTransform() throws Exception {
 		return "success";
 	}
-	public String docIntegrate() throws Exception{
-		DocumentDAO dd=new DocumentDAO();
-		list=dd.selectfile(((Members)session.get("members")).getMemberno());
+
+	public String docIntegrate() throws Exception {
+		DocumentDAO dd = new DocumentDAO();
+		list = dd.selectfile(((Members) session.get("members")).getMemberno());
 		return "success";
 	}
-	public String docForm() throws Exception{
+
+	public String docForm() throws Exception {
 		DocumentDAO dao = new DocumentDAO();
 		docFormList = dao.primaryFormList();
 		return "success";
 	}
-	public String fileShow() throws Exception{
-		System.out.println("DocumentAction:fileShow():save_fileno="+save_fileno);
+
+	public String fileShow() throws Exception {
+		System.out.println("DocumentAction:fileShow():save_fileno=" + save_fileno);
 		DocumentDAO dao = new DocumentDAO();
 		files = dao.fileshow(save_fileno);
-		//excel.Converter.createPDF();
-		//this.convert();
+		// excel.Converter.createPDF();
+		// this.convert();
 		save_file = files.getSave_file();
 		int lastIndex = save_file.lastIndexOf('.');
- 		if (lastIndex == -1){
- 			filename_pdf = "";
- 		}else{ 
- 			filename_pdf = save_file.substring(0, lastIndex);
- 		}
- 		System.out.println("DocumentAction:pdfcreate() filename_pdf:"+filename_pdf);
+		if (lastIndex == -1) {
+			filename_pdf = "";
+		} else {
+			filename_pdf = save_file.substring(0, lastIndex);
+		}
+		System.out.println("DocumentAction:pdfcreate() filename_pdf:" + filename_pdf);
 		return SUCCESS;
 	}
-	
-	public String pdfcreate() throws Exception{
+
+	public String pdfcreate() throws Exception {
 		System.out.println("DocumentAction:pdfcreate() in");
-		System.out.println("save_file:"+save_file+", save_filename:"+save_filename);
-		//DocumentDAO dao = new DocumentDAO();
-		//files = dao.selectfileone(save_fileno);
-		Converter2 con = new Converter2(); //pdf파일생성2줄
+		System.out.println("save_file:" + save_file + ", save_filename:" + save_filename);
+		// DocumentDAO dao = new DocumentDAO();
+		// files = dao.selectfileone(save_fileno);
+		Converter2 con = new Converter2(); // pdf파일생성2줄
 		con.convert2(save_file, save_filename);
-		
-		
- 		int lastIndex = save_file.lastIndexOf('.');
- 		if (lastIndex == -1){
- 			filename_pdf = "";
- 		}else{ 
- 			filename_pdf = save_file.substring(0, lastIndex)+".pdf";
- 		}
- 		System.out.println("DocumentAction:pdfcreate() filename_pdf:"+filename_pdf);
+
+		int lastIndex = save_file.lastIndexOf('.');
+		if (lastIndex == -1) {
+			filename_pdf = "";
+		} else {
+			filename_pdf = save_file.substring(0, lastIndex) + ".pdf";
+		}
+		System.out.println("DocumentAction:pdfcreate() filename_pdf:" + filename_pdf);
 		return SUCCESS;
 	}
-	public String insertfile() throws Exception{
-		if(uploadFileName.contains(".xlsx")==false){
-			msg="2007버전 이상의 엑셀 파일을 올려야 합니다!";
+
+	public String insertfile() throws Exception {
+		if (uploadFileName.contains(".xlsx") == false) {
+			msg = "2007버전 이상의 엑셀 파일을 올려야 합니다!";
 			return "error";
 		}
-		if (upload != null) { 
+		if (upload != null) {
 			FileService fs = new FileService();
-			String basePath = "C:/upload";		//user.properties에 지정된 파일 저장 경로
-			String savedfile = fs.saveFile(upload, basePath, uploadFileName);	//서버에 임시 업로드된 파일객체, 저장경로, 업로드당시의 파일명 전달하고 실제 저장된 파일명 리턴받음
+			String basePath = "C:/upload"; // user.properties에 지정된 파일 저장 경로
+			String savedfile = fs.saveFile(upload, basePath, uploadFileName); // 서버에
+																				// 임시
+																				// 업로드된
+																				// 파일객체,
+																				// 저장경로,
+																				// 업로드당시의
+																				// 파일명
+																				// 전달하고
+																				// 실제
+																				// 저장된
+																				// 파일명
+																				// 리턴받음
 			files.setSave_file(savedfile);
 			files.setSave_filename(uploadFileName);
 		}
-		DocumentDAO dd=new DocumentDAO();
+		DocumentDAO dd = new DocumentDAO();
 		dd.insertfile(files);
 		return "success";
 	}
-	
-	public String insertfile_docform() throws Exception{
-		if (upload != null) { 
+
+	public String insertfile_docform() throws Exception {
+		if (upload != null) {
 			FileService fs = new FileService();
-			String basePath = "C:/upload";		//user.properties에 지정된 파일 저장 경로
-			String savedfile = fs.saveFile(upload, basePath, uploadFileName);	//서버에 임시 업로드된 파일객체, 저장경로, 업로드당시의 파일명 전달하고 실제 저장된 파일명 리턴받음
+			String basePath = "C:/upload"; // user.properties에 지정된 파일 저장 경로
+			String savedfile = fs.saveFile(upload, basePath, uploadFileName); // 서버에
+																				// 임시
+																				// 업로드된
+																				// 파일객체,
+																				// 저장경로,
+																				// 업로드당시의
+																				// 파일명
+																				// 전달하고
+																				// 실제
+																				// 저장된
+																				// 파일명
+																				// 리턴받음
 			files.setSave_file(savedfile);
 			files.setSave_filename(uploadFileName);
 		}
-		DocumentDAO dd=new DocumentDAO();
+		DocumentDAO dd = new DocumentDAO();
 		dd.insertfile(files);
 		save_file = files.getSave_file();
 		save_filename = files.getSave_filename();
-		
-		Converter2 con = new Converter2(); //pdf파일생성2줄
+
+		Converter2 con = new Converter2(); // pdf파일생성2줄
 		con.convert2(save_file, save_filename);
-		
- 		int lastIndex = save_file.lastIndexOf('.');
+
+		int lastIndex = save_file.lastIndexOf('.');
 		filename_pdf = save_file.substring(0, lastIndex);
- 		System.out.println("DocumentAction:insert_docform() filename_pdf:"+filename_pdf);
+		System.out.println("DocumentAction:insert_docform() filename_pdf:" + filename_pdf);
 		return "success";
 	}
-	public String delfile() throws Exception{
-		DocumentDAO dd=new DocumentDAO();
+
+	public String insertword() throws Exception {
+
+		if (upload != null) {
+			FileService fs = new FileService();
+			String basePath = "C:/upload"; // user.properties에 지정된 파일 저장 경로
+			String savedfile = fs.saveFile(upload, basePath, uploadFileName); // 서버에
+																				// 임시
+																				// 업로드된
+																				// 파일객체,
+																				// 저장경로,
+																				// 업로드당시의
+																				// 파일명
+																				// 전달하고
+																				// 실제
+																				// 저장된
+																				// 파일명																				// 리턴받음
+			files.setSave_file(savedfile);
+			files.setSave_filename(uploadFileName);
+		}
+
+		DocumentDAO dd = new DocumentDAO();
+		dd.insertfile(files);
+		return "success";
+
+	}
+
+	public String delfile() throws Exception {
+		DocumentDAO dd = new DocumentDAO();
 		FileService fs = new FileService();
-		String fullpath="C:/upload/"+dd.searchfile(uploadFileName);	
+		String fullpath = "C:/upload/" + dd.searchfile(uploadFileName);
 		fs.fileDelete(fullpath);
 		dd.delfile(uploadFileName);
 		return "success";
 	}
 
-	public String makefile() throws Exception{
-		String[]array=uploadFileName.split(",");
-		ReadExcelDemo ex=new ReadExcelDemo();
-		ExcelMain em=new ExcelMain();
-		DocumentDAO dd=new DocumentDAO();
-		String p="";
-		double j,k=0;
-		ArrayList<String>goods=new ArrayList<String>();
-		ArrayList<String>size=new ArrayList<String>();
-		ArrayList<Double>hob=new ArrayList<Double>();
-		ArrayList<Double>month=new ArrayList<Double>();
-		ArrayList<Double>day=new ArrayList<Double>();
-		ArrayList<Double>kazu=new ArrayList<Double>();
-		ArrayList<Double>cost=new ArrayList<Double>();
-		ArrayList<Double>all=new ArrayList<Double>();
-		
-		for(int i=0;i<array.length;i++){
-			k+=ex.number(dd.searchfile(array[i]), 8, 4);
+	public String makefile() throws Exception {
+		String[] array = uploadFileName.split(",");
+		ReadExcelDemo ex = new ReadExcelDemo();
+		ExcelMain em = new ExcelMain();
+		DocumentDAO dd = new DocumentDAO();
+		String p = "";
+		double j, k = 0;
+		ArrayList<String> goods = new ArrayList<String>();
+		ArrayList<String> size = new ArrayList<String>();
+		ArrayList<Double> hob = new ArrayList<Double>();
+		ArrayList<Double> month = new ArrayList<Double>();
+		ArrayList<Double> day = new ArrayList<Double>();
+		ArrayList<Double> kazu = new ArrayList<Double>();
+		ArrayList<Double> cost = new ArrayList<Double>();
+		ArrayList<Double> all = new ArrayList<Double>();
+
+		for (int i = 0; i < array.length; i++) {
+			k += ex.number(dd.searchfile(array[i]), 8, 4);
 		}
 		hob.add(k);
-			
-		for(int i=1;i<array.length;i++){
-			for(int q=0;q<23;q++){
-				if(ex.word(dd.searchfile(array[i]), 11+q, 2).equals("")){
+
+		for (int i = 1; i < array.length; i++) {
+			for (int q = 0; q < 23; q++) {
+				if (ex.word(dd.searchfile(array[i]), 11 + q, 2).equals("")) {
 					break;
 				}
-					goods.add(ex.word(dd.searchfile(array[i]), 11+q, 2));
-					size.add(ex.word(dd.searchfile(array[i]), 11+q, 13));
-					month.add(ex.number(dd.searchfile(array[i]), 11+q,0));
-					day.add(ex.number(dd.searchfile(array[i]), 11+q,1));
-					kazu.add(ex.number(dd.searchfile(array[i]), 11+q,16));
-					cost.add(ex.number(dd.searchfile(array[i]), 11+q,19));
-					all.add(ex.number(dd.searchfile(array[i]), 11+q,25));
-				
+				goods.add(ex.word(dd.searchfile(array[i]), 11 + q, 2));
+				size.add(ex.word(dd.searchfile(array[i]), 11 + q, 13));
+				month.add(ex.number(dd.searchfile(array[i]), 11 + q, 0));
+				day.add(ex.number(dd.searchfile(array[i]), 11 + q, 1));
+				kazu.add(ex.number(dd.searchfile(array[i]), 11 + q, 16));
+				cost.add(ex.number(dd.searchfile(array[i]), 11 + q, 19));
+				all.add(ex.number(dd.searchfile(array[i]), 11 + q, 25));
+
 			}
 
 		}
-		
-		ArrayList<String>date=new ArrayList<String>();
-		date.add(ex.word(dd.searchfile(array[0]), 1, 0)+"~"+ex.word(dd.searchfile(array[array.length-1]), 1, 0));
-		
-		ArrayList<ArrayList>result=new ArrayList<ArrayList>();
-		ArrayList<ArrayList>receive=new ArrayList<ArrayList>();
+
+		ArrayList<String> date = new ArrayList<String>();
+		date.add(ex.word(dd.searchfile(array[0]), 1, 0) + "~" + ex.word(dd.searchfile(array[array.length - 1]), 1, 0));
+
+		ArrayList<ArrayList> result = new ArrayList<ArrayList>();
+		ArrayList<ArrayList> receive = new ArrayList<ArrayList>();
 		receive.add(date);
 		receive.add(goods);
 		receive.add(size);
@@ -210,120 +252,179 @@ public class DocumentAction extends ActionSupport implements SessionAware{
 		result.add(kazu);
 		result.add(cost);
 		result.add(all);
-		integrate=em.paste(ex.copy(dd.searchfile(array[0])),result,receive);
+		integrate = em.paste(ex.copy(dd.searchfile(array[0])), result, receive);
 		return "success";
 	}
-	public String doctransform()throws Exception{
+
+	public String changefile() throws Exception {
+		System.out.println("찍히는건가");
+		String[] array = uploadFileName.split(",");
+		System.out.println(array);
+
+		DocumentDAO dd = new DocumentDAO();
+		String save_filename = dd.searchfile(array[0]);
+
+		System.out.println(save_filename);
+
+		ReadWord rw = new ReadWord();
+		// ReadWord.readDocFile(save_filename);
+
+		ArrayList<String[]> room = new ArrayList<>();
+
+		room = rw.readWord(dd.searchfile(array[0]));
+
+		ExcelReadWrite erw = new ExcelReadWrite();
+
+		erw.write(room);
+
 		return "success";
+
 	}
+
+	public String doctransform() throws Exception {
+		DocumentDAO dd = new DocumentDAO();
+		list = dd.selectfile(((Members) session.get("members")).getMemberno());
+		return "success";
+
+	}
+
 	public Files getFiles() {
 		return files;
 	}
+
 	public void setFiles(Files files) {
 		this.files = files;
 	}
+
 	public File getUpload() {
 		return upload;
 	}
+
 	public void setUpload(File upload) {
 		this.upload = upload;
 	}
+
 	public String getUploadFileName() {
 		return uploadFileName;
 	}
+
 	public void setUploadFileName(String uploadFileName) {
 		this.uploadFileName = uploadFileName;
 	}
+
 	public String getUploadContentType() {
 		return uploadContentType;
 	}
+
 	public void setUploadContentType(String uploadContentType) {
 		this.uploadContentType = uploadContentType;
 	}
+
 	public List<Files> getList() {
 		return list;
 	}
+
 	public void setList(List<Files> list) {
 		this.list = list;
 	}
+
 	@Override
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
-		
+
 	}
+
 	public Members getMembers() {
 		return members;
 	}
+
 	public void setMembers(Members members) {
 		this.members = members;
 	}
+
 	public List<Files> getDocFormList() {
 		return docFormList;
 	}
+
 	public void setDocFormList(List<Files> docFormList) {
 		this.docFormList = docFormList;
 	}
+
 	public String getSave_fileno() {
 		return save_fileno;
 	}
+
 	public void setSave_fileno(String save_fileno) {
 		this.save_fileno = save_fileno;
 	}
+
 	public String getSave_file() {
 		return save_file;
 	}
+
 	public void setSave_file(String save_file) {
 		this.save_file = save_file;
 	}
+
 	public String getIntegrate() {
 		return integrate;
 	}
+
 	public void setIntegrate(String integrate) {
 		this.integrate = integrate;
 	}
+
 	public String getMsg() {
 		return msg;
 	}
+
 	public void setMsg(String msg) {
 		this.msg = msg;
 	}
+
 	public String getSave_filename() {
 		return save_filename;
 	}
+
 	public void setSave_filename(String save_filename) {
 		this.save_filename = save_filename;
 	}
+
 	public String getFilename_pdf() {
 		return filename_pdf;
 	}
+
 	public void setFilename_pdf(String filename_pdf) {
 		this.filename_pdf = filename_pdf;
 	}
-/*	public DocCustomizing getaNewCreate() {
-		return aNewCreate;
-	}
-	public void setaNewCreate(DocCustomizing aNewCreate) {
-		this.aNewCreate = aNewCreate;
-	}*/
+
+	/*
+	 * public DocCustomizing getaNewCreate() { return aNewCreate; } public void
+	 * setaNewCreate(DocCustomizing aNewCreate) { this.aNewCreate = aNewCreate;
+	 * }
+	 */
 	public List<Files> getCreateFiles() {
 		return createFiles;
 	}
+
 	public void setCreateFiles(List<Files> createFiles) {
 		this.createFiles = createFiles;
 	}
+
 	public String getArr() {
 		return arr;
 	}
+
 	public void setArr(String arr) {
 		this.arr = arr;
 	}
+
 	public String[] getCreateList() {
 		return createList;
 	}
+
 	public void setCreateList(String[] createList) {
 		this.createList = createList;
 	}
-	
-	
-	
+
 }
--- src/excel/ReadWord.java
+++ src/excel/ReadWord.java
@@ -1,29 +1,300 @@
-//package excel;
-//
-//import java.io.FileInputStream;
-//
-//import org.apache.poi.openxml4j.opc.OPCPackage;
-//import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
-//import org.apache.poi.xwpf.usermodel.XWPFDocument;
-//import org.apache.poi.xwpf.usermodel.XWPFFooter;
-//import org.apache.poi.xwpf.usermodel.XWPFHeader;
-//
-//public class ReadWord {
-//			
-//		   public static void main(String[] args) {
-//		     try {
-//			 FileInputStream fis = new FileInputStream("D:/31��.doc");
-//			 XWPFDocument xdoc=new XWPFDocument(OPCPackage.open(fis));
-//			 XWPFHeaderFooterPolicy policy = new XWPFHeaderFooterPolicy(xdoc);
-//			 //read header
-//			 XWPFHeader header = policy.getDefaultHeader();
-//			 System.out.println(header.getText());
-//			 //read footer
-//			 XWPFFooter footer = policy.getDefaultFooter();
-//			 System.out.println(footer.getText());
-//		     } catch(Exception ex) {
-//			ex.printStackTrace();
-//		     } 
-//		  }
-//		}
-//
+package excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFFooter;
+import org.apache.poi.xwpf.usermodel.XWPFHeader;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+
+public class ReadWord {
+
+	public ArrayList<String[]> readWord(String save_filename) {
+
+		try {
+			FileInputStream fis = new FileInputStream("C:/Upload/" + save_filename);
+			// read header
+
+			XWPFDocument document = new XWPFDocument(fis);
+
+			List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+			System.out.println("Total no of paragraph " + paragraphs.size());
+			for (XWPFParagraph para : paragraphs) {
+				System.out.println(para.getText());
+			}
+			fis.close();
+
+			
+			
+			List<XWPFTable> table = document.getTables();
+			
+			
+
+			for (XWPFTable xwpfTable : table) {
+				
+				List<XWPFTableRow> row = xwpfTable.getRows();
+				
+				
+				
+//				System.out.println(row.get(2).getTableCells().get(0).getText());
+//				System.out.println(row.get(2).getTableCells().get(1).getText());
+//				System.out.println(row.get(2).getTableCells().get(2).getText());
+//				System.out.println(row.get(2).getTableCells().get(3).getText());
+//				System.out.println(row.get(2).getTableCells().get(4).getText());
+//				System.out.println(row.get(2).getTableCells().get(5).getText());
+//				System.out.println(row.get(2).getTableCells().get(6).getText());
+//				System.out.println(row.get(2).getTableCells().get(7).getText());
+//				System.out.println(row.get(2).getTableCells().get(8).getText());
+//				System.out.println(row.get(2).getTableCells().get(9).getText());
+//				System.out.println(row.get(2).getTableCells().get(10).getText());
+
+				ArrayList<String[]> room = new ArrayList<>();
+				
+				String[] data1 = new String[11];
+				String[] data2 = new String[11];
+				String[] data3 = new String[11];
+				String[] data4 = new String[11];
+				String[] data5 = new String[11];
+				String[] data6 = new String[11];
+				String[] data7 = new String[11];
+				String[] data8 = new String[11];
+				String[] data9 = new String[11];
+				String[] data10 = new String[11];
+				String[] data11 = new String[11];
+				String[] data12 = new String[11];
+				String[] data13 = new String[11];
+				String[] data14 = new String[11];
+				String[] data15 = new String[11];
+				String[] data16 = new String[11];
+				String[] data17 = new String[11];
+				String[] data18 = new String[11];
+				String[] data19 = new String[11];
+				String[] data20 = new String[11];
+				
+				
+				for (int i = 0; i < data1.length; i++) {
+					data1[i] = row.get(2).getTableCells().get(i).getText()+" ";
+					System.out.print(data1[i]);
+				}
+				
+
+				for (int i = 0; i < data2.length; i++) {
+					data2[i] = row.get(3).getTableCells().get(i).getText()+" ";
+					System.out.print(data2[i]);
+				}
+				
+				for (int i = 0; i < data3.length; i++) {
+					data3[i] = row.get(4).getTableCells().get(i).getText()+" ";
+					System.out.print(data3[i]);
+				}
+				
+				for (int i = 0; i < data4.length; i++) {
+					data4[i] = row.get(5).getTableCells().get(i).getText()+" ";
+					System.out.print(data4[i]);
+				}
+		
+				
+				for (int i = 0; i < data5.length; i++) {
+					data5[i] = row.get(6).getTableCells().get(i).getText()+" ";
+					System.out.print(data5[i]);
+				}
+				
+
+				for (int i = 0; i < data6.length; i++) {
+					data6[i] = row.get(7).getTableCells().get(i).getText()+" ";
+					System.out.print(data6[i]);
+				}
+				
+				for (int i = 0; i < data7.length; i++) {
+					data7[i] = row.get(8).getTableCells().get(i).getText()+" ";
+					System.out.print(data7[i]);
+				}
+				
+				for (int i = 0; i < data8.length; i++) {
+					data8[i] = row.get(9).getTableCells().get(i).getText()+" ";
+					System.out.print(data8[i]);
+				}
+				
+				
+				for (int i = 0; i < data9.length; i++) {
+					data9[i] = row.get(10).getTableCells().get(i).getText()+" ";
+					System.out.print(data1[9]);
+				}
+				
+
+				for (int i = 0; i < data10.length; i++) {
+					data10[i] = row.get(11).getTableCells().get(i).getText()+" ";
+					System.out.print(data10[i]);
+				}
+				
+				for (int i = 0; i < data11.length; i++) {
+					data11[i] = row.get(12).getTableCells().get(i).getText()+" ";
+					System.out.print(data11[i]);
+				}
+				
+				for (int i = 0; i < data12.length; i++) {
+					data12[i] = row.get(13).getTableCells().get(i).getText()+" ";
+					System.out.print(data12[i]);
+				}
+				
+				
+				for (int i = 0; i < data13.length; i++) {
+					data13[i] = row.get(14).getTableCells().get(i).getText()+" ";
+					System.out.print(data13[i]);
+				}
+				
+
+				for (int i = 0; i < data14.length; i++) {
+					data14[i] = row.get(15).getTableCells().get(i).getText()+" ";
+					System.out.print(data14[i]);
+				}
+				
+				for (int i = 0; i < data15.length; i++) {
+					data15[i] = row.get(16).getTableCells().get(i).getText()+" ";
+					System.out.print(data15[i]);
+				}
+				
+				for (int i = 0; i < data16.length; i++) {
+					data16[i] = row.get(17).getTableCells().get(i).getText()+" ";
+					System.out.print(data16[i]);
+				}
+				
+				for (int i = 0; i < data17.length; i++) {
+					data17[i] = row.get(18).getTableCells().get(i).getText()+" ";
+					System.out.print(data17[i]);
+				}
+				
+				for (int i = 0; i < data18.length; i++) {
+					data15[i] = row.get(17).getTableCells().get(i).getText()+" ";
+					System.out.print(data18[i]);
+				}
+				
+				for (int i = 0; i < data19.length; i++) {
+					data16[i] = row.get(18).getTableCells().get(i).getText()+" ";
+					System.out.print(data19[i]);
+				}
+				
+				for (int i = 0; i < data20.length; i++) {
+					data17[i] = row.get(19).getTableCells().get(i).getText()+" ";
+					System.out.print(data20[i]);
+				}
+				
+				
+				
+				room.add(data1);
+				room.add(data2);
+				room.add(data3);
+				room.add(data4);
+				room.add(data5);
+				room.add(data6);
+				room.add(data7);
+				room.add(data8);
+				room.add(data9);
+				room.add(data10);
+				room.add(data11);
+				room.add(data12);
+				room.add(data13);
+				room.add(data14);
+				room.add(data15);
+				room.add(data16);
+				room.add(data17);
+				room.add(data18);
+				room.add(data19);
+				room.add(data20);
+				
+				System.out.println();
+				
+				return room;
+				
+				// 2번째 줄 data를 data 배열에 넣는것 까지 성공하였다
+				
+				
+				
+				
+				
+				
+				
+//				for (XWPFTableRow xwpfTableRow : row) {
+//					
+//					List<XWPFTableCell> cell = xwpfTableRow.getTableCells();
+//					
+//					for (XWPFTableCell xwpfTableCell : cell) {
+//					
+//						if (xwpfTableCell != null) {
+//							
+//							xwpfTableCell.getText();
+//							
+//							System.out.print(xwpfTableCell.getText());
+							
+							
+							
+							
+							
+//							List<XWPFTable> itable = xwpfTableCell.getTables();
+//							
+//							if (itable.size() != 0) {
+//								
+//								for (XWPFTable xwpfiTable : itable) {
+//									
+//									List<XWPFTableRow> irow = xwpfiTable.getRows();
+//									
+//									for (XWPFTableRow xwpfiTableRow : irow) {
+//										
+//										List<XWPFTableCell> icell = xwpfiTableRow.getTableCells();
+//										
+//										for (XWPFTableCell xwpfiTableCell : icell) {
+//											
+//											if (xwpfiTableCell != null) {
+//												
+////												System.out.print(xwpfiTableCell.getText());
+//											}
+//										}
+//										
+//									}
+//								}
+//							}
+//						}
+//					}
+//				}
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("C:/Upload/" + "20161025.docx");
+			// read header
+
+			XWPFDocument document = new XWPFDocument(fis);
+
+			List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+			System.out.println("Total no of paragraph " + paragraphs.size());
+			for (XWPFParagraph para : paragraphs) {
+				System.out.println(para.getText());
+			}
+			fis.close();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
