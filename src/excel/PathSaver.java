2d9418863b2c89679c1c215dfacefc3bf1f7c6fe
HR <HR@HR-PC>
2016-10-28 08:38:11 +0900

    real commit ^오^

--- src/excel/PathSaver.java
+++ src/excel/PathSaver.java
@@ -10,7 +10,7 @@ public class PathSaver {
 	//이게 저장되어 있는 주소라고 친다면, 
 	//C:/ServerUtils/workspace_projectDocFairy/ 이곳이 자신의 eclipse의 workspace의 주소입니다. 이 위치를 자신이 사용중인 workspace경로로 바꾸시면 됩니다.
 	public static String filePath(){ //톰캣에 올라가진 실제 파일 주소
-		return "C:/FinalProject/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/project";
+		return "C:/Users/HR/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/project";
 	}
 	
 	public static String librePath(){ //libreOffice가 저장된 주소
