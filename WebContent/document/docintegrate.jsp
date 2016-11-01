54d92754243c86cfd60efa4d30cfe953d1df66ed
pkm6335 <pjj6335@naver.com>
2016-11-01 21:04:39 +0900

    dd

--- WebContent/document/docintegrate.jsp
+++ WebContent/document/docintegrate.jsp
@@ -209,10 +209,10 @@ $(function(){
 				<a href="javascript:pagingFormSubmit(${pagenavi.currentPage + 1})">&gt;</a>
 	
 			</p>
-			<s:form name="pagingForm" method="post" action="listBoard" theme="simple">
+			<s:form name="pagingForm" method="post" action="docintegrate" theme="simple">
 			<p class="board_search">
 	<s:hidden name="currentPage" value="%{pagenavi.currentPage}" />
-	<s:textfield  name="searchText" />
+	<s:textfield name="searchText" />
 	<a href="javascript:pagingFormSubmit(1)">검색</a>
 			</p>
 </s:form>
--- src/action/DocumentAction.java
+++ src/action/DocumentAction.java
@@ -93,14 +93,14 @@ public class DocumentAction extends ActionSupport implements SessionAware {
 
 	public String docIntegrate() throws Exception {
 		DocumentDAO dd = new DocumentDAO();
-		int countPerPage = 10;		//페이지당 글목록 수
+		int countPerPage = 5;		//페이지당 글목록 수
 		int pagePerGroup = 5;		//그룹당 페이지 수
 		//전체 글수 구하기
 		int total = dd.getTotal(searchText);	
 		//PageNavigator 객체 생성 (페이지당 글수, 그룹당 페이지 수, 현재 페이지, 전체 글수)
 		pagenavi = new PageNavigator(countPerPage, pagePerGroup, currentPage, total);		
 		//현재 페이지에 해당하는 글 목록 읽기 (전체 레코드 중 보여줄 첫번째 글의 위치, 페이지당 글 수 )
-		list = dd.selectfile(searchText, pagenavi.getStartRecord(), pagenavi.getCountPerPage(),((Members) session.get("members")).getMemberno());
+		list = dd.selectfile(((Members) session.get("members")).getMemberno(), pagenavi.getStartRecord(), pagenavi.getCountPerPage(),searchText);
 		return "success";
 	}
 
